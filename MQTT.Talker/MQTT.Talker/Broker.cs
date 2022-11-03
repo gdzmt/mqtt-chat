ccf2479b1ae1f2bfc0ef8a9ccc0bac23819cb14a
fa19374 <albafelipe@johndeere.com>
2022-11-03 01:11:11 -0600

    Template

---
+++ MQTT.Talker/MQTT.Talker/Broker.cs
@@ -0,0 +1,9 @@
+﻿namespace MQTT.Talker
+{
+    public class Broker
+    {
+        public string Server { get; set; }
+
+        public int Port { get; set; }
+    }
+}
---
+++ MQTT.Talker/MQTT.Talker/MQTTConnection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MQTT.Talker
+{
+    internal class MQTTConnection
+    {
+
+       
+        public void Connect(Broker broker, bool setEvents = true)
+        {
+            
+
+        }
+
+        public void Disconnect()
+        {
+            
+        }
+
+        private MqttClient CreateClient(Broker broker, bool setEvents = true)
+        {
+            return null;
+        }
+
+        public void Send(string message, string destinationName)
+        {
+            
+        }
+
+        public void Subscribe(string sourceName)
+        {
+            
+        }
+
+        public void Unsubscribe(string sourceName)
+        {
+            
+        }
+
+        private void MqttMessageReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            
+
+        }
+
+    }
+}
